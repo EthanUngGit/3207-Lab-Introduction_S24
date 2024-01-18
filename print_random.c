02b1578229d018a17f484c17fd3b5fca8c70d886
EthanUngGit <ethan.ung@temple.edu>
2024-01-18 15:31:05 -0500

    EthanUngPrintRandC

--- print_random.c
+++ print_random.c
@@ -9,13 +9,12 @@ int main()
 	int a;
 
 	/* seed the randomizer */
-	srand( (unsigned)time(NULL) );
+	srand((unsigned)time(NULL));
 
 	printf("Today's random word: ");
 	for(a=0;a<7;a++)
-		putchar( randchar() );
+		putchar(randchar());
 	putchar('\n');
 
 	return(0);
 }
-
